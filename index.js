8deae65ca914996296756fc2c1a16a0101cb69d7
Joao Santos <joao.santos@vizir.com.br>
2016-12-26 11:02:34 -0200

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 var SmartBot = require('./lib/smart-bot');
 
 var config = {
-	pageAccessToken: 'YOUR-PAGE-ACCESS-TOKEN',
-	verifyToken: 'YOUR-VERIFY-TOKEN'
+	pageAccessToken: process.env.PAGE_ACCESS_TOKEN,
+	verifyToken: process.env.VERIFY_TOKEN
 };
 
 smartBot = new SmartBot(config);
@@ -18,29 +19,29 @@ smartBot.step('menu', SmartBot.STEP_OPTION_MENU,{
 	options: [
 		{
 			label: 'option 1',
-			goTo: 
+			goTo: 'option-1-selected'
 		},
 		{
 			label: 'option 2',
-			goTo: optionTwoStep
+			goTo: 'option-2-selected'
 		}
 	]
 });
 
-smartBot.step('Say you select option 1',SmartBot.STEP_SENTENCE,{
+smartBot.step('option-1-selected',SmartBot.STEP_SENTENCE,{
 	message: 'Say you select option 1',
-	goTo: goodbyeStep,
+	goTo: 'option-1-selected',
 	sleep: 3
 });
 
-smartBot.step('Say you select option 2',SmartBot.STEP_SENTENCE,{
+smartBot.step('option-2-selected',SmartBot.STEP_SENTENCE,{
 	message: 'Say you select option 2',
-	goTo: goodbyeStep,
+	goTo: 'option-2-selected',
 	sleep: 3
 });
 
-smartBot.step('Say goodbye',SmartBot.STEP_SENTENCE,{
+smartBot.step('goodbye',SmartBot.STEP_SENTENCE,{
 	message: 'Goodbye!!!',
 });
 
-smartBot.goTo(firstStep);
+smartBot.goTo('first-step');
